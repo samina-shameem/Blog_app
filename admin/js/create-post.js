bfcd3afabdfe501b00eb8513656904d860fb21e6
samina-shameem <samina.shameem.clt@gmail.com>
2023-12-14 20:19:13 +0100

    create new post and delete post

---
+++ admin/js/create-post.js
@@ -0,0 +1,42 @@
+document.getElementById('blogPostForm').addEventListener('submit', createPost);
+
+async function createPost(event) {
+    event.preventDefault();
+
+    let form = event.target;
+
+    // Form validation
+    if (!form.checkValidity()) {
+        alert('Please fill out all required fields.');
+        return;
+    }
+
+    try {
+        let formData = new FormData(form);
+        let data = {
+            "title": formData.get('title'),
+            "author": formData.get('author'),
+            "content": formData.get('content'),
+            "tags": formData.getAll('tags'), 
+        };
+        console.log(data);
+        // Perform the POST request using async/await
+        const response = await fetch('https://blog-api-assignment.up.railway.app/posts', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        location.replace('index.html');
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
--- admin/js/manage-posts.js
+++ admin/js/manage-posts.js
@@ -1,62 +1,85 @@
-fetchAllBlog();
-
+// Function to fetch and display blogs
 async function fetchAllBlog() {
     try {
         const response = await fetch('https://blog-api-assignment.up.railway.app/posts');
         const blogs = await response.json();
 
-        let blogTableHTML = `<table >
-  <tr>
-    <th>Title</th>
-    <th>Author</th>
-    <th>Tags</th>
-    <th>Date</th>
-    <th>Action</th>
-  </tr>  
-`;
-        for (let blog of blogs) {          
-          
-          console.log(blog);
+        let blogTableHTML = `<table>
+            <tr>
+                <th>Title</th>
+                <th>Author</th>
+                <th>Tags</th>
+                <th>Date</th>
+                <th>Action</th>
+            </tr>`;
 
-            let content = blog.content.substring(0, 100);
-            // let content = blog.content;
-            let blogDate = new Date(blog.date)
+        for (let blog of blogs) {
+            let blogDate = new Date(blog.date);
             let strTags = "";
-            if (blog.tags){
+
+            if (blog.tags) {
                 for (let tag of blog.tags) {
-                    strTags += tag + ",";
+                    strTags += tag + ", ";
                 }
             }
 
-            strTags = strTags.slice(0,-1);
+            strTags = strTags.slice(0, -2);
+
             blogTableHTML += `
                 <tr>
                     <td>${blog.title}</td>
                     <td>${blog.author}</td>
                     <td>${strTags}</td>
-                    <td>${blogDate.getFullYear()}-${blogDate.getMonth()+1}-${blogDate.getDate()} ${blogDate.toLocaleTimeString()}</td>
-                    <td> <a href="update-pun.html?id=${blog._id}">Update</a> - <a href="#" data-id="${blog._id}" class="delete-links">Delete</a></td>
-                </tr>`
-
-            // blogListHTML += `
-            //     <li class="list-group-item">
-            //         <h2>${blog.title}</h2> <br>
-            //         <p><span class="date"> ${blog.author} | ${blogDate.getFullYear()}-${blogDate.getMonth()+1}-${blogDate.getDate()} ${blogDate.toLocaleTimeString()}</span> </p>                    
-            //         <p>${content} <p> 
-            //         <a href="#">read more</a>
-            //         <p>Tags : ${strTags} <p> 
-
-            //         <div>
-            //             <a href="update-pun.html?id=${blog._id}">Update</a> |
-            //             <a href="#" data-id="${blog._id}" class="delete-links">Delete</a> 
-            //         </div>
-            //     </li>
-            // `
+                    <td>${blogDate.getFullYear()}-${blogDate.getMonth() + 1}-${blogDate.getDate()} ${blogDate.toLocaleTimeString()}</td>
+                    <td> <a href="update-post.html?id=${blog._id}">Update</a> <a href="#" data-id="${blog._id}" class="delete-links">Delete</a></td>
+                </tr>`;
         }
 
         blogTableHTML += `</table>`;
         document.getElementById('blog-list').innerHTML = blogTableHTML;
-    } catch(error) {
-        console.log(error)
-    }   
+
+        // Now that the blogs are added, attach event listeners to delete links
+        attachDeleteEventListeners();
+    } catch (error) {
+        console.log(error);
+    }
 }
+
+// Function to attach event listeners to delete links
+function attachDeleteEventListeners() {
+    // Get all delete links
+    const deleteLinks = document.getElementsByClassName('delete-links');
+
+    // Attach click event listeners to each delete link
+    for (const deleteLink of deleteLinks) {
+        deleteLink.addEventListener('click', function (event) {
+            event.preventDefault();
+            const blogId = event.target.dataset.id;
+            deleteBlogPost(blogId);
+        });
+    }
+}
+
+// Function to delete a blog post
+async function deleteBlogPost(blogId) {
+    try {
+        const response = await fetch(`https://blog-api-assignment.up.railway.app/posts/${blogId}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        // If successful, reload the blog list
+        fetchAllBlog();
+    } catch (error) {
+        console.log('Error:', error);
+    }
+}
+
+// Fetch and display blogs when the page loads
+fetchAllBlog();
